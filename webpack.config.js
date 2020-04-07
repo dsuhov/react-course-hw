e5d5b8a1dd9f7b29f14e486ed0f3afb7e882f915
Dmitriy Sukhov <dsujev@gmail.com>
2020-04-08 02:08:05 +0300

    trying husky

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/app.ts',
+  entry: "./src/app.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
   },
   devtool: "source-map",
   module: {
@@ -13,17 +13,17 @@ module.exports = {
       {
         test: /\.(js|ts)?$/,
         loader: "babel-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   /*
-  * If multiple files share the same name but have different extensions, 
-  * webpack will resolve the one with the extension listed first 
-  * in the array and skip the rest.
-  */
+   * If multiple files share the same name but have different extensions,
+   * webpack will resolve the one with the extension listed first
+   * in the array and skip the rest.
+   */
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
   },
   devServer: {
     // When using the HTML5 History API, the index.html page will likely have to be served in place of any 404 responses.
@@ -32,7 +32,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      minify: false
+      minify: false,
     }),
-  ]
-}
+  ],
+};
