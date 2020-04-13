eaece4f16328a40f0626394226f883bba58c333f
Dmitriy Sukhov <dsujev@gmail.com>
2020-04-13 16:35:34 +0300

    Добавил тесты jest и настройки loki

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 interface Prop {
-    username?: string;  
+  username?: string;
 }
 
 const HelloWorld: React.FC<Prop> = (props) => {
-    return <h2>Hello World, {props.username}</h2>;
-}
+  return <h2>Hello World, {props.username}</h2>;
+};
 
 export default HelloWorld;
