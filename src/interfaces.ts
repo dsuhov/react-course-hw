0c99a46d66536a76854f1232f0aedaecf4195aa1
Dmitriy Sukhov <dsujev@gmail.com>
2020-05-02 16:21:13 +0300

    clean old files, temp disable hooks

--- src/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface EntryCardProps {
-  disabled?: boolean;
-  definition: string;
-  meaning: string;
-  entryClickHandler: (isDisabled: boolean, id: number) => void;
-  id: number;
-}
