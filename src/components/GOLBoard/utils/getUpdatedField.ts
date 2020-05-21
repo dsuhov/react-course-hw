917507c0cb60ad39f6615596938af318310df135
Dmitriy Sukhov <dstujev@gmail.com>
2020-05-21 18:36:15 +0300

    refactored ControlsArea, added some tests, optimized getUpdatedField

--- src/components/GOLBoard/utils/getUpdatedField.ts
+++ src/components/GOLBoard/utils/getUpdatedField.ts
@@ -1,34 +1,22 @@
-function adjacentStatus(field: boolean[][], coords: [number, number]): 1 | 0 {
-  const x = coords[0];
-  const y = coords[1];
-
-  if (x < 0 || x > field[0].length - 1) {
-    return 0;
-  }
-  if (y < 0 || y > field.length - 1) {
-    return 0;
-  }
-
-  return field[y][x] ? 1 : 0;
-}
-
 function livecircleStatus(
   cell: boolean,
   cellCoords: [number, number],
   field: boolean[][]
 ): boolean {
   let adjacentAlive = 0;
-  const x = cellCoords[0];
-  const y = cellCoords[1];
+  const x = cellCoords[1];
+  const y = cellCoords[0];
 
-  adjacentAlive += adjacentStatus(field, [x - 1, y - 1]);
-  adjacentAlive += adjacentStatus(field, [x, y - 1]);
-  adjacentAlive += adjacentStatus(field, [x + 1, y - 1]);
-  adjacentAlive += adjacentStatus(field, [x - 1, y]);
-  adjacentAlive += adjacentStatus(field, [x + 1, y]);
-  adjacentAlive += adjacentStatus(field, [x - 1, y + 1]);
-  adjacentAlive += adjacentStatus(field, [x, y + 1]);
-  adjacentAlive += adjacentStatus(field, [x + 1, y + 1]);
+  adjacentAlive += field[x - 1] ? (field[x - 1][y - 1] ? 1 : 0) : 0;
+  adjacentAlive += field[x][y - 1] ? 1 : 0;
+  adjacentAlive += field[x + 1] ? (field[x + 1][y - 1] ? 1 : 0) : 0;
+
+  adjacentAlive += field[x - 1] ? (field[x - 1][y] ? 1 : 0) : 0;
+  adjacentAlive += field[x + 1] ? (field[x + 1][y] ? 1 : 0) : 0;
+
+  adjacentAlive += field[x - 1] ? (field[x - 1][y + 1] ? 1 : 0) : 0;
+  adjacentAlive += field[x][y + 1] ? 1 : 0;
+  adjacentAlive += field[x + 1] ? (field[x + 1][y + 1] ? 1 : 0) : 0;
 
   if (cell) {
     if (adjacentAlive < 2) {
