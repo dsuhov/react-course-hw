b427a6114db42eacb2d4e71610975e37def32d26
Dmitriy Sukhov <dsujev@gmail.com>
2020-06-07 19:12:27 +0300

    modified react redux DrawField tests

--- src/components/GOLBoard/components/DrawField/DrawField.test.tsx
+++ src/components/GOLBoard/components/DrawField/DrawField.test.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { mount } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import renderer from "react-test-renderer";
 import { DrawField } from "./DrawField";
 import { Cell } from "./components";
+import { Provider } from "react-redux";
+import { golField as reducer } from "@/rdx/reducers/golField";
+import { createStore, Reducer } from "redux";
 
 function newTestScheme(
   xSize: number,
@@ -16,70 +19,98 @@ function newTestScheme(
   );
 }
 
+/**
+ * Main sense of this test is to check that DrawField component
+ * correctly renders correct amount of cells in correct configureation and
+ * passes correct data to them.
+ */
+
+const setupWrapper = (
+  reducer: Reducer<FieldScheme>,
+  initState: FieldScheme
+): ReactWrapper => {
+  const rootState =
+    {
+      golField: initState,
+      gameStatus: { status: "stopped" },
+    } as any;
+
+  const newStore = createStore(reducer, rootState);
+  const field = mount(
+    <Provider store={newStore}>
+      <DrawField />
+    </Provider>
+  );
+
+  return field;
+};
+
 describe("Field that draw cells", () => {
-  // const emptyFieldScheme = newTestScheme(4, 5, false);
-  // const fullFieldScheme = newTestScheme(4, 5, true);
+  const emptyFieldScheme = newTestScheme(4, 5, false) as FieldScheme;
+  const fullFieldScheme = newTestScheme(4, 5, true) as FieldScheme;
 
-  // it("reders full empty field", () => {
-  //   expect(renderer.create(<DrawField />).toJSON()).toMatchSnapshot();
-  // });
+  it("reders full empty field", () => {
+    const wrapper = setupWrapper(reducer, emptyFieldScheme);
+    expect(renderer.create(wrapper.get(0)).toJSON()).toMatchSnapshot();
+  });
 
-  // it("reders full alive field", () => {
-  //   expect(renderer.create(<DrawField />).toJSON()).toMatchSnapshot();
-  // });
+  it("reders full alive field", () => {
+    const wrapper = setupWrapper(reducer, emptyFieldScheme);
+    expect(renderer.create(wrapper.get(0)).toJSON()).toMatchSnapshot();
+  });
 
-  // it("renders empty field 4x5", () => {
-  //   const field = mount(<DrawField />);
+  it("renders empty field 4x5", () => {
+    const wrapper = setupWrapper(reducer, emptyFieldScheme);
 
-  //   const fieldSize = emptyFieldScheme[0].length * emptyFieldScheme.length;
+    const fieldSize = emptyFieldScheme[0].length * emptyFieldScheme.length;
 
-  //   expect(
-  //     field.findWhere((el) => el.is(Cell) && el.props().isAlive === false)
-  //       .length
-  //   ).toBe(fieldSize);
-  // });
+    expect(
+      wrapper.findWhere((el) => el.is(Cell) && el.props().isAlive === false)
+        .length
+    ).toBe(fieldSize);
+  });
 
-  // it("renders full field 4x5", () => {
-  //   const field = mount(<DrawField />);
+  it("renders full field 4x5", () => {
+    const field = setupWrapper(reducer, fullFieldScheme);
 
-  //   const fieldSize = fullFieldScheme[0].length * fullFieldScheme.length;
+    const fieldSize = fullFieldScheme[0].length * fullFieldScheme.length;
 
-  //   expect(
-  //     field.findWhere((el) => el.is(Cell) && el.props().isAlive === true).length
-  //   ).toBe(fieldSize);
-  // });
+    expect(
+      field.findWhere((el) => el.is(Cell) && el.props().isAlive === true).length
+    ).toBe(fieldSize);
+  });
 
-  // it("renders partial filled field, size 3*4", () => {
-  //   const partialFilledField = [
-  //     [false, true, true, true],
-  //     [true, false, false, false],
-  //     [true, true, false, true],
-  //     [false, true, false, true],
-  //   ];
+  it("renders partial filled field, size 3*4", () => {
+    const partialFilledField = [
+      [false, true, true, true],
+      [true, false, false, false],
+      [true, true, false, true],
+      [false, true, false, true],
+    ];
 
-  //   const [aliveCells, deadCells] = partialFilledField.reduce(
-  //     (result, current) => {
-  //       return [
-  //         result[0] + current.filter((el) => el === true).length,
-  //         result[1] + current.filter((el) => el === false).length,
-  //       ];
-  //     },
-  //     [0, 0]
-  //   );
+    const [aliveCells, deadCells] = partialFilledField.reduce(
+      (result, current) => {
+        return [
+          result[0] + current.filter((el) => el === true).length,
+          result[1] + current.filter((el) => el === false).length,
+        ];
+      },
+      [0, 0]
+    );
 
-  //   const field = mount(<DrawField />);
+    const field = setupWrapper(reducer, partialFilledField);
 
-  //   expect(
-  //     field.findWhere((el) => el.is(Cell) && el.props().isAlive === false)
-  //       .length
-  //   ).toBe(deadCells);
+    expect(
+      field.findWhere((el) => el.is(Cell) && el.props().isAlive === false)
+        .length
+    ).toBe(deadCells);
 
-  //   expect(
-  //     field.findWhere((el) => el.is(Cell) && el.props().isAlive === true).length
-  //   ).toBe(aliveCells);
+    expect(
+      field.findWhere((el) => el.is(Cell) && el.props().isAlive === true).length
+    ).toBe(aliveCells);
 
-  //   expect(
-  //     field.findWhere((el) => el.is(Cell) && el.props().isAlive === true).length
-  //   ).not.toBe(1);
-  // });
+    expect(
+      field.findWhere((el) => el.is(Cell) && el.props().isAlive === true).length
+    ).not.toBe(1);
+  });
 });
