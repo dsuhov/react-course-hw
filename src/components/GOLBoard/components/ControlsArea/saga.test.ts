b056997ec35c74e36111648b62d662b0e0fc0edf
Dmitriy Sukhov <dsujev@gmail.com>
2020-06-24 01:06:55 +0300

    add saga test for ControlsArea

--- src/components/GOLBoard/components/ControlsArea/saga.test.ts
+++ src/components/GOLBoard/components/ControlsArea/saga.test.ts
@@ -1,16 +1,23 @@
 import { expectSaga } from "redux-saga-test-plan";
 import { gameStatusSlice } from "@/rdx/gameStatus/gameStatusSlice";
 import { gameFieldSlice } from "@/rdx/gameField/gameFieldSlice";
-import { watchUpdateField } from "./saga";
+import { incGeneration } from "./saga";
 import { takeEvery } from "redux-saga/effects";
 import { put } from "redux-saga-test-plan/matchers";
 
 const { reducer } = gameStatusSlice;
-const { updateField } = gameFieldSlice.actions;
+const { incGen } = gameStatusSlice.actions;
 
 describe("Controls Area saga test", ()=> {
   it("watch inc gen", () => {
-    // expectSaga(watchUpdateField)
-    //   .take(takeEvery(updateField()));
+    return expectSaga(incGeneration)
+      .withReducer(reducer)
+      .put(incGen())
+      .hasFinalState({
+        status: "stopped",
+        interval: 200,
+        generation: 1,
+      })
+      .run();
   });
 });
--- src/components/GOLBoard/components/ControlsArea/saga.ts
+++ src/components/GOLBoard/components/ControlsArea/saga.ts
@@ -2,10 +2,10 @@ import { put, takeEvery, call } from "redux-saga/effects";
 import { gameFieldActions } from "@/rdx/gameField/gameFieldSlice";
 import { gameStatusActions } from "@/rdx/gameStatus/gameStatusSlice";
 
-function* UpdField() {
+export function* incGeneration() {
   yield put(gameStatusActions.incGen());
 }
 
 export function* watchUpdateField() {
-  yield takeEvery(gameFieldActions.updateField.type, UpdField);
+  yield takeEvery(gameFieldActions.updateField.type, incGeneration);
 }
